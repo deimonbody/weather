c3f451926a134e15032b7ea00de089a218a3b41a
deimonBody <pavlov.d@donnu.edu.ua>
2022-10-21 15:10:45 +0300

    Create base react app ,configured eslint file and create layout by html+css

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchLine from "../SearchLine/SearchLine";
+import { MainPart } from "../MainPart/MainPart";
+import "./style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchLine />
+      <MainPart />
+    </div>
+  );
+}
+
+export default App;
